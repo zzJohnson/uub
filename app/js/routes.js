4dcee8f1773031fb240cabfa1416095d13cb0fb3
heziyang1993 <ziyongho@qq.com>
2017-08-04 20:06:57 +0800

    'merge'

--- app/js/routes.js
+++ app/js/routes.js
@@ -1,8 +1,10 @@
 ;(function(){
 	var routes = angular.module('routes',[]);
-//	routes.config(function($stateProvider,$urlRouterProvider){
-//		$stateProvider.state('',{
-//			url:'',
+	routes.config(function($stateProvider,$urlRouterProvider){
+		$stateProvider.state('index',{
+			url:'/index',
 //			templateUrl:''
-//		})
+		})
+		$urlRouterProvider.otherwise('/index/home')
+	})
 })();
