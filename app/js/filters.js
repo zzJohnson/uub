f79afcb3115162e32c151dcf98353c8c8b0c718c
wuqian052412 <937584186@qq.com>
2017-08-07 19:00:53 +0800

    更改详情页时间戳问题

--- app/js/filters.js
+++ app/js/filters.js
@@ -1,8 +1,13 @@
 ;(function(){
 	var filters = angular.module('filters',[]);
-	filters.filter('',function(){
-		return function(){
-			
+	//更改时间戳过滤器
+	filters.filter('time',function(){
+		return function(input){
+			var oDate1 = new Date(input);
+			var oDate = new Date();
+			var nTime = oDate.getTime() - oDate1.getTime();  
+			var hour = Math.floor(nTime/1000/60/60); 
+			return hour
 		}
 	})
 })();
